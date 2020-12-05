e81d3f1df7090116234971c7483f043dbd52a5e2
Chevy Ray Johnston <happytrash@gmail.com>
2020-12-04 23:14:36 -0800

    updated day4 to print out both part1 and part2 answers

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = include_str!("input.txt");
 
-    let valid_count = input
+    let values: Vec<(usize, usize)> = input
         .split("\n\n")
         .map(|group| {
             group
@@ -12,59 +12,72 @@ fn main() {
                     Some((pair.next()?, pair.next()?))
                 })
                 .map(|(key, val)| match key {
-                    "byr" => {
+                    "byr" => (
+                        true,
                         val.len() == 4
                             && val
                                 .parse::<usize>()
                                 .and_then(|y| Ok(y >= 1920 && y <= 2002))
-                                .unwrap_or(false)
-                    }
-                    "iyr" => {
+                                .unwrap_or(false),
+                    ),
+                    "iyr" => (
+                        true,
                         val.len() == 4
                             && val
                                 .parse::<usize>()
                                 .and_then(|y| Ok(y >= 2010 && y <= 2020))
-                                .unwrap_or(false)
-                    }
-                    "eyr" => {
+                                .unwrap_or(false),
+                    ),
+                    "eyr" => (
+                        true,
                         val.len() == 4
                             && val
                                 .parse::<usize>()
                                 .and_then(|y| Ok(y >= 2020 && y <= 2030))
-                                .unwrap_or(false)
-                    }
-                    "hgt" => (&val[0..val.len() - 2])
-                        .parse::<usize>()
-                        .and_then(|h| {
-                            Ok(match &val[(val.len() - 2)..] {
-                                "cm" => h >= 150 && h <= 193,
-                                "in" => h >= 59 && h <= 76,
-                                _ => false,
+                                .unwrap_or(false),
+                    ),
+                    "hgt" => (
+                        true,
+                        (&val[0..val.len() - 2])
+                            .parse::<usize>()
+                            .and_then(|h| {
+                                Ok(match &val[(val.len() - 2)..] {
+                                    "cm" => h >= 150 && h <= 193,
+                                    "in" => h >= 59 && h <= 76,
+                                    _ => false,
+                                })
                             })
-                        })
-                        .unwrap_or(false),
-                    "hcl" => {
+                            .unwrap_or(false),
+                    ),
+                    "hcl" => (
+                        true,
                         val.len() == 7
                             && val
                                 .find('#')
                                 .and_then(|i| {
                                     Some(i == 0 && usize::from_str_radix(&val[1..], 16).is_ok())
                                 })
-                                .unwrap_or(false)
-                    }
-                    "ecl" => match val {
-                        "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
-                        _ => false,
-                    },
-                    "pid" => val.len() == 9 && val.chars().all(|c| c.is_numeric()),
-                    "cid" => false,
-                    _ => false,
+                                .unwrap_or(false),
+                    ),
+                    "ecl" => (
+                        true,
+                        match val {
+                            "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+                            _ => false,
+                        },
+                    ),
+                    "pid" => (true, val.len() == 9 && val.chars().all(|c| c.is_numeric())),
+                    "cid" => (false, false),
+                    _ => (false, false),
                 })
-                .filter(|&v| v)
-                .count()
+                .map(|(a, b)| (if a { 1 } else { 0 }, if b { 1 } else { 0 }))
+                .fold((0, 0), |(na, nb), (a, b)| (na + a, nb + b))
         })
-        .filter(|&v| v >= 7)
-        .count();
+        .collect();
 
-    println!("valid passports: {}", valid_count);
+    let valid1 = values.iter().filter(|(a, _)| a >= &7).count();
+    println!("part 1: {}", valid1);
+
+    let valid2 = values.iter().filter(|(_, b)| b >= &7).count();
+    println!("part 2: {}", valid2);
 }
