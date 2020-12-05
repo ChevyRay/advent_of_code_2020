202f1bd5e89bfbae5af683695e271562d00cc9ea
Chevy Ray Johnston <happytrash@gmail.com>
2020-12-05 10:16:01 -0800

    cleaned up day5, removing binary partition repetition

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,26 +1,18 @@
 fn main() {
     let input = include_str!("day5.txt");
 
+    fn search(line: &str, range: usize) -> usize {
+        line.chars()
+            .fold((0, range), |(a, b), chr| match chr {
+                'F' | 'L' => (a, b - (b - a) / 2),
+                _ => (a + (b - a) / 2, b),
+            })
+            .0
+    }
+
     let mut ids: Vec<usize> = input
         .lines()
-        .map(|line| {
-            (
-                line[0..7]
-                    .chars()
-                    .fold((0, 128), |(a, b), chr| match chr {
-                        'F' => (a, b - (b - a) / 2),
-                        _ => (a + (b - a) / 2, b),
-                    })
-                    .0,
-                line[7..10]
-                    .chars()
-                    .fold((0, 8), |(a, b), chr| match chr {
-                        'L' => (a, b - (b - a) / 2),
-                        _ => (a + (b - a) / 2, b),
-                    })
-                    .0,
-            )
-        })
+        .map(|line| (search(&line[0..7], 128), search(&line[7..10], 8)))
         .map(|(row, col)| (row * 8 + col))
         .collect();
 
@@ -29,11 +21,12 @@ fn main() {
 
     ids.sort();
 
-    let my_id = *ids[0..ids.len() - 1]
+    let my_id = 1 + *ids[0..ids.len() - 1]
         .iter()
         .enumerate()
         .find(|(i, id)| *id + 1 != ids[i + 1])
-        .unwrap().1 + 1;
+        .unwrap()
+        .1;
 
     println!("my id: {}", my_id);
 }
