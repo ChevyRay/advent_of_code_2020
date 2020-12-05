b053d678a8170cab4568782e2650c8bf3165d404
Chevy Ray Johnston <happytrash@gmail.com>
2020-12-04 22:30:04 -0800

    updated day1 solution to remove dependency and hand-coded the combination function

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,48 +1,34 @@
-use itertools::Itertools;
-
 fn main() {
-    let input_str = include_str!("input.txt");
-    let input: Vec<usize> = input_str
-        .split('\n')
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect();
+    let input = include_str!("input.txt");
 
-    // PART 1:
-    //  1) find the 2 numbers that sum to 2020
-    //  2) multiply those numbers
-    if let [a, b] = (input
-        .iter()
-        .combinations(2)
-        .find(|n| n[0] + n[1] == 2020)
-        .unwrap())[..]
-    {
-        println!(
-            "result:\n\t{} + {} = 2020\n\t{} * {} = {}",
-            a,
-            b,
-            a,
-            b,
-            a * b
-        );
-    }
+    let nums: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
 
-    // PART 2:
-    //  1) find 3 numbers that follow the same criteria as part 1
-    if let [a, b, c] = (input
+    let (a, b) = nums
         .iter()
-        .combinations(3)
-        .find(|n| n[0] + n[1] + n[2] == 2020)
-        .unwrap())[..]
-    {
-        println!(
-            "result:\n\t{} + {} + {} = 2020\n\t{} * {} * {} = {}",
-            a,
-            b,
-            c,
-            a,
-            b,
-            c,
-            a * b * c
-        );
-    }
+        .enumerate()
+        .map(|(i, a)| {
+            let rest = &nums[(i + 1)..];
+            rest.iter().map(move |b| (*a, *b))
+        })
+        .flatten()
+        .find(|(a, b)| a + b == 2020)
+        .unwrap();
+
+    println!("first: {}", a * b);
+
+    let (a, b, c) = nums
+        .iter()
+        .enumerate()
+        .map(|(i, a)| {
+            let rest = &nums[(i + 1)..];
+            rest.iter()
+                .enumerate()
+                .map(move |(i, b)| rest[(i + 1)..].iter().map(move |c| (*a, *b, *c)))
+                .flatten()
+        })
+        .flatten()
+        .find(|(a, b, c)| a + b + c == 2020)
+        .unwrap();
+
+    println!("second: {}", a * b * c);
 }
