57eb808d2b8f9d66dbcf4248ca8e12165836539e
Chevy Ray Johnston <happytrash@gmail.com>
2020-12-04 22:59:59 -0800

    updated day3 puzzle to be much simpler and functional

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,65 +1,29 @@
 fn main() {
-    let hill = {
-        let input = include_str!("input.txt");
-        let cols = input.find('\n').unwrap();
-        let trees: Vec<bool> = input.replace('\n', "").chars().map(|c| c == '#').collect();
-        let rows = trees.len() / cols;
-        Hill {
-            size: (cols, rows),
-            trees,
-        }
-    };
+    let input = include_str!("input.txt");
+    let cols = input.find('\n').unwrap();
+    let hill: Vec<usize> = input
+        .lines()
+        .map(|line| line.chars())
+        .flatten()
+        .map(|chr| if chr == '.' { 0 } else { 1 })
+        .collect();
+    let rows = hill.len() / cols;
 
-    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let slopes: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
-    let result: usize = slopes
+    let product: usize = slopes
         .iter()
-        .map(|s| {
-            let count = hill.count_trees(*s);
-            println!("slope ({},{}) = {} trees", s.0, s.1, count);
-            count
+        .map(|(sx, sy)| {
+            let t = (0..rows)
+                .step_by(*sy)
+                .fold((0, 0), |(x, trees), y| {
+                    (x + sx, trees + hill[y * cols + (x % cols)])
+                })
+                .1;
+            println!("{},{} = {}", sx, sy, t);
+            t
         })
         .product();
 
-    println!("product of tree counts: {}", result);
-}
-
-struct Hill {
-    size: (usize, usize),
-    trees: Vec<bool>,
-}
-
-impl Hill {
-    fn has_tree(&self, p: &(usize, usize)) -> bool {
-        self.trees[p.1 * self.size.0 + p.0]
-    }
-    fn count_trees(&self, slope: (usize, usize)) -> usize {
-        Slope {
-            pos: (0, 0),
-            size: self.size,
-            slope,
-        }
-        .filter(|t| self.has_tree(t))
-        .count()
-    }
-}
-
-struct Slope {
-    pos: (usize, usize),
-    slope: (usize, usize),
-    size: (usize, usize),
-}
-
-impl Iterator for Slope {
-    type Item = (usize, usize);
-    fn next(&mut self) -> Option<Self::Item> {
-        let p = self.pos;
-        if p.1 < self.size.1 {
-            self.pos.0 = (self.pos.0 + self.slope.0) % self.size.0;
-            self.pos.1 += self.slope.1;
-            Some(p)
-        } else {
-            None
-        }
-    }
+    println!("product: {}", product);
 }
