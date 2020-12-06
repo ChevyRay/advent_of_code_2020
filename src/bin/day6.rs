4061265295d0abe1f753f8af4ff3023d8494e3a7
Chevy Ray Johnston <happytrash@gmail.com>
2020-12-06 00:21:16 -0800

    moved group split to top

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,19 +1,16 @@
 fn main() {
     let input = include_str!("day6.txt");
+    let groups: Vec<&str> = input.split("\n\n").collect();
 
-    let sum: usize = input
-        .split("\n\n")
-        .map(|group| {
-            ('a'..='z')
-                .filter(|chr| group.contains(*chr))
-                .count()
-        })
+    let sum: usize = groups
+        .iter()
+        .map(|group| ('a'..='z').filter(|chr| group.contains(*chr)).count())
         .sum();
 
     println!("part 1: {}", sum);
 
-    let sum: usize = input
-        .split("\n\n")
+    let sum: usize = groups
+        .iter()
         .map(|group| group.lines().collect::<Vec<&str>>())
         .map(|group| {
             ('a'..='z')
